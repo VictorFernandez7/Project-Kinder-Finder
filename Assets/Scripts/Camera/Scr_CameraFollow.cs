d8a134da06a98e506fb30dc46c1711e327bc1d6b
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-10-29 17:46:50 +0100

    Camera Rotation - 17:46

--- Assets/Scripts/Camera/Scr_CameraFollow.cs
+++ Assets/Scripts/Camera/Scr_CameraFollow.cs
@@ -9,20 +9,23 @@ public class Scr_CameraFollow : MonoBehaviour
     [SerializeField] private float zoomInPlanet;
     [SerializeField] private float zoomSpeed;
     [SerializeField] private float zoomDistance;
+    [SerializeField] private float rotationSpeed;
 
     [Header("References")]
     [SerializeField] private GameObject playerShip;
     [SerializeField] private GameObject astronaut;
 
     [HideInInspector] public bool followAstronaut = true;
+    [HideInInspector] public bool smoothRotation;
 
     public GameObject currentPlanet;
     private Camera mainCamera;
 
+    private Vector3 playerShipVectorUp;
+    private Vector3 astronauUpVector;
     private float astronautRotation;
     private float currentRotation;
     private bool tookOff;
-    private bool changeRotation;
 
     private void Start()
     {
@@ -37,22 +40,39 @@ public class Scr_CameraFollow : MonoBehaviour
     private void Update()
     {
         if (followAstronaut == false)
+        {
             transform.position = new Vector3(playerShip.transform.position.x, playerShip.transform.position.y, -10);
+
+            if (smoothRotation)
+            {
+                playerShipVectorUp = playerShip.transform.up;
+
+                transform.up = Vector3.Lerp(transform.up, playerShipVectorUp, Time.deltaTime * rotationSpeed);
+            }
+        }
+
 
         else
         {
             transform.position = new Vector3(astronaut.transform.position.x, astronaut.transform.position.y, -10);
-            changeRotation = true;
-            
-        }
+            //smoothRotation = true;
 
-        if (changeRotation)
-        {
-            astronautRotation = astronaut.transform.rotation.eulerAngles.z;
-            currentRotation = transform.rotation.eulerAngles.z;
-            transform.rotation = Quaternion.Euler(0f, 0f, astronautRotation);
+            if (!smoothRotation)
+            {
+                astronautRotation = astronaut.transform.rotation.eulerAngles.z;
 
-            //transform.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(currentRotation, astronautRotation, Time.deltaTime));
+                transform.rotation = Quaternion.Euler(0f, 0f, astronautRotation);
+            }
+
+            else
+            {
+                if (smoothRotation)
+                {
+                    astronauUpVector = astronaut.transform.up;
+
+                    transform.up = Vector3.Lerp(transform.up, astronauUpVector, Time.deltaTime * rotationSpeed);
+                }
+            }
         }
 
         if (Vector3.Distance(currentPlanet.transform.position, playerShip.transform.position) > zoomDistance && mainCamera.orthographicSize < zoomInSpace)
