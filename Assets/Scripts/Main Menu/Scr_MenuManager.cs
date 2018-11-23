5745c394d3bddb0cdae1191c09e0ac8c53274862
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-11-23 14:38:55 +0100

    Main menu part 1 - 14:38

--- Assets/Scripts/Main Menu/Scr_MenuManager.cs
+++ Assets/Scripts/Main Menu/Scr_MenuManager.cs
@@ -5,29 +5,88 @@ using UnityEngine;
 
 public class Scr_MenuManager : MonoBehaviour
 {
-    [Header("Menu Parameters")]
-    [SerializeField] private float cameraSpeed;
+    [Header("Button Detection")]
+    [SerializeField] private LayerMask layerMask;
+
+    [Header("Animations")]
+    [SerializeField] public Animation playAnimation;
+    [SerializeField] public Animation controlsAnimation;
+    [SerializeField] public Animation aboutUsAnimation;
+    [SerializeField] public Animation extitAnimation;
 
     private GameObject mainCamera;
+    private GameObject targetPlanet;
 
     private void Start()
     {
         mainCamera = GameObject.Find("MainCamera");
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        CameraMovement();
+        RaycastHit buttonHit;
+        Ray ratCastToMousePos = mainCamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ratCastToMousePos, out buttonHit, layerMask))
+        {
+            Debug.DrawLine(mainCamera.transform.position, buttonHit.point, Color.yellow);
+
+            targetPlanet = buttonHit.transform.gameObject;
+
+            targetPlanet.GetComponent<Animator>().SetBool("Rotate", true);
+
+            if (buttonHit.transform.gameObject.CompareTag("PlayButton"))
+                playAnimation["Anim_PlayButtonRotation"].speed = 1;
+
+            else if (buttonHit.transform.gameObject.CompareTag("ControlsButton"))
+                controlsAnimation["Anim_ControlButtonRotate"].speed = 1;
+
+            else if (buttonHit.transform.gameObject.CompareTag("AboutUsButton"))
+                aboutUsAnimation["Anim_AboutUsButtonRotate"].speed = 1;
+
+            else if (buttonHit.transform.gameObject.CompareTag("ExitButton"))
+                extitAnimation["Anim_ExitButtonRotate"].speed = 1;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (buttonHit.transform.gameObject.CompareTag("PlayButton"))
+                    PlayGame();
+
+                else if (buttonHit.transform.gameObject.CompareTag("ControlsButton"))
+                    ControlsPlanet();
+
+                else if (buttonHit.transform.gameObject.CompareTag("AboutUsButton"))
+                    AboutUsPlanet();
+
+                else if (buttonHit.transform.gameObject.CompareTag("ExitButton"))
+                    ExitGame();
+            }
+        }
+
+        else if (targetPlanet != null)
+        {
+            playAnimation["Anim_PlayButtonRotation"].speed = 0;
+            controlsAnimation["Anim_ControlButtonRotate"].speed = 0;
+            aboutUsAnimation["Anim_AboutUsButtonRotate"].speed = 0;
+            extitAnimation["Anim_ExitButtonRotate"].speed = 0;
+        }
+
+        print(targetPlanet);
     }
 
-    private void CameraMovement()
-    {
-        mainCamera.transform.position += new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"), 0) * Time.deltaTime * cameraSpeed;
-    }
-
-    public void LoadNextScene()
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void ControlsPlanet()
+    {
+
+    }
+
+    private void AboutUsPlanet()
+    {
+
     }
 
     public void ExitGame()
