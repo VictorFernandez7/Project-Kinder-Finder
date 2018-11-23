ed097e3bd6acda103dcaf9d2a460b0047024a0d9
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-11-23 17:45:56 +0100

    Cambio planetas menú funcional - 17:45

--- Assets/Scripts/Main Menu/Scr_MenuManager.cs
+++ Assets/Scripts/Main Menu/Scr_MenuManager.cs
@@ -6,61 +6,95 @@ using UnityEngine;
 public class Scr_MenuManager : MonoBehaviour
 {
     [Header("Button Detection")]
-    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private LayerMask buttonMask;
+    [SerializeField] private LayerMask backMask;
+
+    [Header("Camera Settings")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform controlsCamSpot;
+    [SerializeField] private Transform controlsBackSpot;
+    [SerializeField] private Transform aboutUsCamSpot;
+    [SerializeField] private Transform aboutUsBackSpot;
 
     [Header("Text References")]
     [SerializeField] private Animator playText;
     [SerializeField] private Animator controlsText;
     [SerializeField] private Animator aboutUsText;
     [SerializeField] private Animator exitText;
+    [SerializeField] private Animator backText;
 
+    private bool moveToSpot;
+    private bool mainMenu;
+    private Vector3 currentCameraSpot;
+    private Vector3 currentBackSpot;
+    private Vector3 initialCameraSpot;
+    private Vector3 initialBackSpot;
     private GameObject mainCamera;
+    private GameObject backPlanet;
     private GameObject targetPlanet;
 
     private void Start()
     {
         mainCamera = GameObject.Find("MainCamera");
+        backPlanet = GameObject.Find("BackPlanet");
+
+        initialCameraSpot = mainCamera.transform.position;
+        initialBackSpot = backPlanet.transform.position;
+
+        mainMenu = true;
     }
 
-    private void FixedUpdate()
+    private void Update()
+    {
+        CheckSelection();
+        CheckInput();
+
+        if (moveToSpot)
+            MoveToSpot();
+    }
+
+    private void CheckSelection()
     {
         RaycastHit buttonHit;
         Ray ratCastToMousePos = mainCamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ratCastToMousePos, out buttonHit, layerMask))
+        if (Physics.Raycast(ratCastToMousePos, out buttonHit, buttonMask))
         {
             Debug.DrawLine(mainCamera.transform.position, buttonHit.point, Color.yellow);
 
             targetPlanet = buttonHit.transform.gameObject;
 
-            targetPlanet.GetComponent<Animator>().SetBool("Rotate", true);
-            targetPlanet.GetComponent<Animator>().SetFloat("Speed", 1);
-
-            if (buttonHit.transform.gameObject.CompareTag("PlayButton"))
-                playText.SetBool("ShowText", true);
-
-            else if (buttonHit.transform.gameObject.CompareTag("ControlsButton"))
-                controlsText.SetBool("ShowText", true);
-
-            else if (buttonHit.transform.gameObject.CompareTag("AboutUsButton"))
-                aboutUsText.SetBool("ShowText", true);
-
-            else if (buttonHit.transform.gameObject.CompareTag("ExitButton"))
-                exitText.SetBool("ShowText", true);
-
-            if (Input.GetMouseButtonDown(0))
+            if (mainMenu)
             {
-                if (buttonHit.transform.gameObject.CompareTag("PlayButton"))
-                    PlayGame();
+                if (!targetPlanet.gameObject.CompareTag("BackButton"))
+                {
+                    targetPlanet.GetComponent<Animator>().SetBool("Rotate", true);
+                    targetPlanet.GetComponent<Animator>().SetFloat("Speed", 1);
+                }
 
-                else if (buttonHit.transform.gameObject.CompareTag("ControlsButton"))
-                    ControlsPlanet();
+                if (targetPlanet.gameObject.CompareTag("PlayButton"))
+                    playText.SetBool("ShowText", true);
 
-                else if (buttonHit.transform.gameObject.CompareTag("AboutUsButton"))
-                    AboutUsPlanet();
+                else if (targetPlanet.gameObject.CompareTag("ControlsButton"))
+                    controlsText.SetBool("ShowText", true);
 
-                else if (buttonHit.transform.gameObject.CompareTag("ExitButton"))
-                    ExitGame();
+                else if (targetPlanet.gameObject.CompareTag("AboutUsButton"))
+                    aboutUsText.SetBool("ShowText", true);
+
+                else if (targetPlanet.gameObject.CompareTag("ExitButton"))
+                    exitText.SetBool("ShowText", true);
+            }
+
+            else
+            {
+                if (targetPlanet.gameObject.CompareTag("BackButton"))
+                {
+                    targetPlanet.GetComponent<Animator>().SetBool("Rotate", true);
+                    targetPlanet.GetComponent<Animator>().SetFloat("Speed", 1);
+                }
+
+                if (targetPlanet.gameObject.CompareTag("BackButton"))
+                    backText.SetBool("ShowText", true);
             }
         }
 
@@ -71,25 +105,81 @@ public class Scr_MenuManager : MonoBehaviour
             controlsText.SetBool("ShowText", false);
             aboutUsText.SetBool("ShowText", false);
             exitText.SetBool("ShowText", false);
+            backText.SetBool("ShowText", false);
         }
     }
 
-    public void PlayGame()
+    private void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (mainMenu)
+            {
+                if (targetPlanet.gameObject.CompareTag("PlayButton"))
+                    PlayGame();
+
+                if (targetPlanet.gameObject.CompareTag("ControlsButton"))
+                    ControlsPlanet();
+
+                if (targetPlanet.gameObject.CompareTag("AboutUsButton"))
+                    AboutUsPlanet();
+
+                if (targetPlanet.gameObject.CompareTag("ExitButton"))
+                    ExitGame();
+            }
+
+            else
+            {
+                if (targetPlanet.gameObject.CompareTag("BackButton"))
+                    Back();
+            }
+        }
+    }
+
+    private void MoveToSpot()
+    {
+        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, currentCameraSpot, Time.deltaTime * moveSpeed);
+
+        if (currentBackSpot == initialBackSpot)
+            backPlanet.transform.position = Vector3.Lerp(backPlanet.transform.position, currentBackSpot, Time.deltaTime * moveSpeed * 3);
+
+        else
+            backPlanet.transform.position = Vector3.Lerp(backPlanet.transform.position, currentBackSpot, Time.deltaTime * moveSpeed);
+    }
+
+    private void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     private void ControlsPlanet()
     {
-
+        moveToSpot = true;
+        mainMenu = false;
+        controlsText.SetBool("ShowText", false);
+        currentCameraSpot = controlsCamSpot.position;
+        currentBackSpot = controlsBackSpot.position;
     }
 
     private void AboutUsPlanet()
     {
-
+        moveToSpot = true;
+        mainMenu = false;
+        aboutUsText.SetBool("ShowText", false);
+        currentCameraSpot = aboutUsCamSpot.position;
+        currentBackSpot = aboutUsBackSpot.position;
     }
 
-    public void ExitGame()
+    private void Back()
+    {
+        moveToSpot = true;
+        mainMenu = true;
+        backText.SetBool("ShowText", false);
+        currentCameraSpot = initialCameraSpot;
+        currentBackSpot = initialBackSpot;
+    }
+
+    private void ExitGame()
     {
         Application.Quit();
     }
