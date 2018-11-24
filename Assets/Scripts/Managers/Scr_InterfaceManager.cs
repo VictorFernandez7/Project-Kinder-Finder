11e11d2da159533d55291e306bf0bb199e36a2e7
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-11-24 20:46:18 +0100

    Animaciones interfaz - 20:46

--- Assets/Scripts/Managers/Scr_InterfaceManager.cs
+++ Assets/Scripts/Managers/Scr_InterfaceManager.cs
@@ -5,31 +5,9 @@ using UnityEngine;
 
 public class Scr_InterfaceManager : MonoBehaviour
 {
-    [Header("Interface Properties")]
-    [SerializeField] private Color disabledColor;
-    [SerializeField] private Color activatedColor;
-
-    [Header("References")]
-    [SerializeField] private GameObject astronautButton;
-    [SerializeField] private GameObject playerShipButton;
-
-    public void ActivateAstronautButton()
-    {
-        astronautButton.GetComponent<Image>().color = activatedColor;
-    }
-
-    public void DeactivateAstronautButton()
-    {
-        astronautButton.GetComponent<Image>().color = disabledColor;
-    }
-
-    public void ActivatePlayerShipButton()
-    {
-        playerShipButton.GetComponent<Image>().color = activatedColor;
-    }
-
-    public void DeactivatePlayerShipButton()
-    {
-        playerShipButton.GetComponent<Image>().color = disabledColor;
-    }
+    [Header("Animators")]
+    [SerializeField] private Animator astronautInterface;
+    [SerializeField] private Animator playerShipInterface;
+    [SerializeField] private Animator playerShipActions;
+    [SerializeField] private Animator playerShipWindow;
 }
