47674e43fbae4e15a6a9907ce85a54382dea7bd2
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-11-25 04:11:21 +0100

    Upgrade script y upgrades space suit funcionales - 4:11

--- Assets/Scripts/Interface/Scr_Upgrade.cs
+++ Assets/Scripts/Interface/Scr_Upgrade.cs
@@ -1,11 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class Scr_Upgrade : MonoBehaviour
 {
-    private void OnMouseDown()
+    [Header("Upgrade Parameters")]
+    [SerializeField] private string upgradeName;
+    [SerializeField] [TextArea] private string description;
+    [SerializeField] private Image descriptionImage;
+    [SerializeField] [TextArea] private string requirements;
+    [SerializeField] [TextArea] private string price;
+
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI upgradeNameText;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private Image descriptionImageImage;
+    [SerializeField] private TextMeshProUGUI requirementsText;
+    [SerializeField] private TextMeshProUGUI priceText;
+
+    public void ApplyTextChange()
     {
-        print("funsiono");
+        upgradeNameText.text = upgradeName;
+        descriptionText.text = description;
+        descriptionImageImage = descriptionImage;
+        requirementsText.text = requirements;
+        priceText.text = price;
     }
 }
