3c9c112b228d4eb1b84ec1fb251003db5409c655
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-11-25 01:22:15 +0100

    Intro screen - 1:22

---
+++ Assets/Scripts/IntroScreen/Scr_IntroScreenManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Scr_IntroScreenManager : MonoBehaviour
+{
+    [Header("Intro Screen Settings")]
+    [SerializeField] private float timeToShowText;
+
+    [Header("References")]
+    [SerializeField] private GameObject continueText;
+
+    private void Update()
+    {
+        timeToShowText -= Time.deltaTime;
+
+        if (timeToShowText <= 0)
+            continueText.SetActive(true);
+
+        if (Input.GetMouseButtonDown(0))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
