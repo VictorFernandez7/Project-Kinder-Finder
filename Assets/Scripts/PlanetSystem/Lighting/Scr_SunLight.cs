0fcc954c262101414b7eaf04b467b0e4cad9e354
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-11-28 09:51:58 +0100

    Luz solar con directional light - 9:51

--- Assets/Scripts/PlanetSystem/Lighting/Scr_SunLight.cs
+++ Assets/Scripts/PlanetSystem/Lighting/Scr_SunLight.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scr_SunLight : MonoBehaviour {
+public class Scr_SunLight : MonoBehaviour
+{
+    private GameObject playerShip;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        playerShip = GameObject.Find("PlayerShip");
+    }
+
+    private void Update()
+    {
+        transform.LookAt(playerShip.transform);
+    }
 }
