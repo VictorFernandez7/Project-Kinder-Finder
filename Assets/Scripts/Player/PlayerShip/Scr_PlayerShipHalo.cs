1a47925f4a8ae8cb0e3ad80fbc24fd43efa1b648
SrAlexen <36672804+SrAlexen@users.noreply.github.com>
2018-12-04 11:15:01 +0100

    ReHaloScript - 11:14

--- Assets/Scripts/Player/PlayerShip/Scr_PlayerShipHalo.cs
+++ Assets/Scripts/Player/PlayerShip/Scr_PlayerShipHalo.cs
@@ -2,15 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scr_Halo : MonoBehaviour {
+public class Scr_PlayerShipHalo : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private LineRenderer haloLine;
+    [SerializeField] private float radius;
+    [SerializeField] private Transform playership;
+
+    // Use this for initialization
+    void Start()
+    {
+        CreateOrbit();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateOrbit();
+    }
+
+    private void CreateOrbit()
+    {
+        int index = 0;
+
+        haloLine.positionCount = 41;
+
+        for (float i = 1; i >= 0; i -= 0.1f)
+        {
+            Vector3 vectorDirector = new Vector3(i, 1 - i, 0);
+            haloLine.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
+            index += 1;
+        }
+
+        for (float i = 0; i >= -1; i -= 0.1f)
+        {
+            Vector3 vectorDirector = new Vector3(i, 1 + i, 0);
+            haloLine.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
+            index += 1;
+        }
+
+        for (float i = -1; i <= 0; i += 0.1f)
+        {
+            Vector3 vectorDirector = new Vector3(i, -1 - i, 0);
+            haloLine.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
+            index += 1;
+        }
+
+        for (float i = 0; i <= 1; i += 0.1f)
+        {
+            Vector3 vectorDirector = new Vector3(i, -1 + i, 0);
+            haloLine.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
+            index += 1;
+        }
+
+        haloLine.SetPosition(40, (new Vector3(1, 0, 0) * radius) + playership.position);
+    }
 }
