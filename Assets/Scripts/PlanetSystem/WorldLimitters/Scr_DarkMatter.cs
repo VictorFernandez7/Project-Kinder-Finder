1050d45a74627a4de2c069edd3a99dac2ba42315
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-12-13 17:25:49 +0100

    Set up dark matter - 17:25

---
+++ Assets/Scripts/PlanetSystem/WorldLimitters/Scr_DarkMatter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_DarkMatter : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private BoxCollider2D boxCollider1;
+    [SerializeField] private BoxCollider2D boxCollider2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerShip"))
+        {
+            if (boxCollider1.enabled)
+            {
+                boxCollider1.enabled = false;
+                boxCollider2.enabled = true;
+            }
+
+            else
+            {
+
+            }
+        }
+    }
+}
