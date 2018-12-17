94cf56e69deadd59d07ca967157c51733ff695dc
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-12-17 09:13:25 +0100

    Parte movimiento - 9:13

---
+++ Assets/Scripts/Tutorial/Scr_TutorialItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_TutorialItem : MonoBehaviour
+{
+    [Header("Set Parent Of")]
+    [SerializeField] private Transform targetParent;
+
+    private void Start()
+    {
+        transform.SetParent(targetParent);
+    }
+}
