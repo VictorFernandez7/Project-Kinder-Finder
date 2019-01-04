c92b172f73f757eeed1b14fb0a1aea51107f2822
SrAlexen <36672804+SrAlexen@users.noreply.github.com>
2019-01-04 14:45:42 +0100

    Inventory Rework - 14:45

--- Assets/Scripts/Player/PlayerShip/Scr_PlayerShipLaboratory.cs
+++ Assets/Scripts/Player/PlayerShip/Scr_PlayerShipLaboratory.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Scr_PlayerShipLaboratory : MonoBehaviour
 { 
     [Header("References")]
     [SerializeField] private Scr_UpgradeList upgradeData;
+    [SerializeField] private Button upgradeButton;
 
     private Scr_PlayerShipCraft playerShipCraft;
     private Scr_PlayerShipStats playerShipStats;
+
+    private int resourceListIndex;
 
     private void Start()
     {
         playerShipCraft = GetComponent<Scr_PlayerShipCraft>();
         playerShipStats = GetComponent<Scr_PlayerShipStats>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+
+        }
     }
 
     public void TechnologyClic(int index)
@@ -22,22 +34,31 @@ public class Scr_PlayerShipLaboratory : MonoBehaviour
         {
             playerShipCraft.InventoryInfo();
 
-            //Codigo activando el boton
+            upgradeButton.interactable = true;
 
             List<string> keyr = new List<string>(playerShipCraft.Resources.Keys);
 
             foreach (string k in keyr)
             {
-                if (upgradeData.UpgradeList[index].Resources[k] > playerShipCraft.Resources[k])
+                for(int p = 0; p < upgradeData.UpgradeList[index].resourceNameList.Count; p++)
                 {
-                    //Codigo desactivando el boton
+                    if(upgradeData.UpgradeList[index].resourceNameList[p] == k)
+                    {
+                        resourceListIndex = p;
+                        break;
+                    }
+                }
+
+                if (upgradeData.UpgradeList[index].resourceAmountList[resourceListIndex] > playerShipCraft.Resources[k])
+                {
+                    upgradeButton.interactable = false;
                     break;
                 }
             }
         }
 
         else
-            Debug.Log(0); //Codigo desactivando el boton
+            upgradeButton.interactable = false;
     }
 
     public void UpgradeButton(int index)
@@ -46,16 +67,28 @@ public class Scr_PlayerShipLaboratory : MonoBehaviour
 
         foreach (string k in keyr)
         {
-            if (upgradeData.UpgradeList[index].Resources[k] > 0)
+            for (int p = 0; p < upgradeData.UpgradeList[index].resourceNameList.Count; p++)
             {
-                for(int i = upgradeData.UpgradeList[index].Resources[k]; i > 0; i--)
+                if (upgradeData.UpgradeList[index].resourceNameList[p] == k)
                 {
-                    for (int j = playerShipStats.resourceWarehouse.Length; j > 0; j--)
+                    resourceListIndex = p;
+                    break;
+                }
+            }
+
+            if (upgradeData.UpgradeList[index].resourceAmountList[resourceListIndex] > 0)
+            {
+                for(int i = upgradeData.UpgradeList[index].resourceAmountList[resourceListIndex]; i > 0; i--)
+                {
+                    for (int j = playerShipStats.resourceWarehouse.Length - 1; j >= 0; j--)
                     {
-                        if(playerShipStats.resourceWarehouse[j].name == k)
+                        if (playerShipStats.resourceWarehouse[j])
                         {
-                            playerShipStats.resourceWarehouse[j] = null;
-                            break;
+                            if (playerShipStats.resourceWarehouse[j].name == k)
+                            {
+                                playerShipStats.resourceWarehouse[j] = null;
+                                break;
+                            }
                         }
                     }
                 }
