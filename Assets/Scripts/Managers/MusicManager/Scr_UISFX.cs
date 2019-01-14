435a5fcefd52fd8638d2628b8517954b4838efd8
Blind <dgo.ego@gmail.com>
2019-01-14 18:22:56 +0100

    Sound Scripts 2/3 - 18:22

--- Assets/Scripts/Managers/MusicManager/Scr_UISFX.cs
+++ Assets/Scripts/Managers/MusicManager/Scr_UISFX.cs
@@ -7,10 +7,75 @@ public class Scr_UISFX : MonoBehaviour
     [Header("Sounds")]
     [SerializeField] private SoundDefinition button;
     [SerializeField] private SoundDefinition buttonLight;
+    [SerializeField] private SoundDefinition addTool;
+    [SerializeField] private SoundDefinition animateIn;
+    [SerializeField] private SoundDefinition animateOut;
+    [SerializeField] private SoundDefinition exitGame;
+    [SerializeField] private SoundDefinition loadGame;
+    [SerializeField] private SoundDefinition newGame;
+    [SerializeField] private SoundDefinition pause;
+    [SerializeField] private SoundDefinition resume;
+    [SerializeField] private SoundDefinition shipUpgrade;
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
-        
+        if ()
+        {
+            Scr_MusicManager.Instance.PlaySound(button.Sound, 0);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlaySound(button.Sound, 1);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlaySound(button.Sound, 2);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlaySound(buttonLight.Sound, 0);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlaySound(buttonLight.Sound, 1);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlaySound(buttonLight.Sound, 2);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlayRandom(animateIn.Sound);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlayRandom(animateOut.Sound);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlaySound(loadGame.Sound, 0);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlayRandom(exitGame.Sound);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlayRandom(newGame.Sound);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlayRandom(pause.Sound);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlayRandom(resume.Sound);
+        }
+        if ()
+        {
+            Scr_MusicManager.Instance.PlayRandom(shipUpgrade.Sound);
+        }
+        */
     }
-}
+
