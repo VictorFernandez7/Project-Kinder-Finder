ed77343f19cf5442d261aeaca6f4ce798e699a4d
VictorFernandez7 <victorfernandez95@hotmail.com>
2019-01-26 14:54:09 +0100

    Nueva versión movimiento IA - 14:54

--- Assets/Scripts/Player/IA/Scr_IAMovement.cs
+++ Assets/Scripts/Player/IA/Scr_IAMovement.cs
@@ -5,23 +5,72 @@ using UnityEngine;
 public class Scr_IAMovement : MonoBehaviour
 {
     [Header("Movement Parameters")]
-    [SerializeField] private float followDistance;
+    [SerializeField] private float rotationSpeed;
+    [Tooltip("Distance from the ship from which the IA will follow the astronaut.")]
+    [SerializeField] private float distanceFormShip;
+    [Tooltip("Movement speed depends on the distance between GameObjects, this is a multiplicator.")]
+    [Range(0, 3)] [SerializeField] private float astronautFollowMult;
+    [Tooltip("Movement speed depends on the distance between GameObjects, this is a multiplicator.")]
+    [Range(0, 3)] [SerializeField] private float playerShipFollowMult;
 
     [Header("References")]
-    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject astronaut;
+    [SerializeField] private Transform astronautSpot;
+    [SerializeField] private GameObject playerShip;
+    [SerializeField] private Transform playerShipSpot;
 
-    private Rigidbody2D rb;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    private float desiredSpeed;
+    private Vector3 desiredRotation;
+    private Transform target;
 
     private void Update()
     {
-        transform.position = (transform.position - player.transform.position).normalized * followDistance + player.transform.position;
-        transform.rotation = player.transform.rotation;
+        CheckPlanet();
+        CheckDistance();        
+    }
 
-        rb.AddForce(player.transform.up * 20);
+    private void FixedUpdate()
+    {
+        FollowTarget();
+    }
+
+    private void CheckPlanet()
+    {
+        transform.SetParent(playerShip.GetComponent<Scr_PlayerShipMovement>().currentPlanet.transform);
+    }
+
+    private void CheckDistance()
+    {
+        if (Vector3.Distance(astronaut.transform.position, playerShip.transform.position) <= distanceFormShip)
+            target = playerShipSpot;
+
+        else
+            target = astronautSpot;
+    }
+
+    private void FollowTarget()
+    {
+        float astronautFollowSpeed = (Vector3.Distance(astronaut.transform.position, transform.position));
+        float playerShipFollowSpeed = (Vector3.Distance(playerShip.transform.position, transform.position));
+        Vector3 astronautUpVector = astronaut.transform.up;
+        Vector3 playerShipVectorUp = playerShip.transform.up;
+
+        if (target == astronautSpot)
+            desiredSpeed = astronautFollowSpeed * astronautFollowMult;
+
+        else
+            desiredSpeed = playerShipFollowSpeed * playerShipFollowMult;
+
+        if (target == astronautSpot)
+            desiredRotation = Vector3.Lerp(desiredRotation, astronautUpVector, Time.deltaTime * rotationSpeed);
+
+        else
+            desiredRotation = Vector3.Lerp(desiredRotation, playerShipVectorUp, Time.deltaTime * rotationSpeed);
+
+        if (target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * desiredSpeed);
+            transform.rotation = Quaternion.LookRotation(transform.forward, desiredRotation);
+        }
     }
 }
