4fadb9e09003666ebec4253574f63666078b14a0
SrAlexen <36672804+SrAlexen@users.noreply.github.com>
2019-01-27 19:35:33 +0100

    Quest Tool - 19:35

--- Assets/Scripts/QuestSystem/Scr_QuestManager.cs
+++ Assets/Scripts/QuestSystem/Scr_QuestManager.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class Scr_QuestManager : MonoBehaviour
 {
-    public Scr_Quest quest = new Scr_Quest();
+    public List<Scr_Quest> quest = new List<Scr_Quest>();
 
     private void Start()
-    {
+    {/*
         //create each event
         Scr_QuestEvent a = quest.AddQuestEvent("test1", "description 1");
         Scr_QuestEvent b = quest.AddQuestEvent("test2", "description 2");
@@ -17,7 +17,7 @@ public class Scr_QuestManager : MonoBehaviour
         Scr_QuestEvent e = quest.AddQuestEvent("test5", "description 5");
 
         //define the paths between the events
-        quest.AddPath(a.GetId(), b.GetId());
+        quest.AddPath(a.GetId(), c.GetId());
         quest.AddPath(b.GetId(), c.GetId());
         quest.AddPath(b.GetId(), e.GetId());
         quest.AddPath(c.GetId(), e.GetId());
@@ -25,6 +25,13 @@ public class Scr_QuestManager : MonoBehaviour
 
         quest.BFS(a.GetId());
 
-        quest.PrintPath();
+        quest.PrintPath();*/
+    }
+
+    private void CreateQuest()
+    {
+        Scr_Quest mission = new Scr_Quest();
+        quest.Add(mission);
+
     }
 }
