4c07a141b718600ecaf9e8ca6d05084cba6a5581
VictorFernandez7 <victorfernandez95@hotmail.com>
2019-02-07 11:28:21 +0100

    Load screen funcional - 11:28

---
+++ Assets/Scripts/LoadingScreen/Scr_Levels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Levels
+{
+    public static LevelToLoad levelToLoad;
+
+    public enum LevelToLoad
+    {
+        PlanetSystem1,
+        PlanetSystem2,
+        PlanetSystem3,
+        PlanetSystem4,
+        PlanetSystem5,
+        PlanetSystem6,
+        PlanetSystem7
+    }
+}
+
+public class LevelStorage
+{
+    public static string planetSystem1 = "PlanetSystem1";
+    public static string planetSystem2 = "PlanetSystem2";
+    public static string planetSystem3 = "PlanetSystem3";
+    public static string planetSystem4 = "PlanetSystem4";
+    public static string planetSystem5 = "PlanetSystem5";
+    public static string planetSystem6 = "PlanetSystem6";
+    public static string planetSystem7 = "PlanetSystem7";
+}
---
+++ Assets/Scripts/LoadingScreen/Scr_LoadingScreen.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Scr_LoadingScreen : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        switch (Scr_Levels.levelToLoad)
+        {
+            case Scr_Levels.LevelToLoad.PlanetSystem1:
+                SceneManager.LoadSceneAsync(LevelStorage.planetSystem1, LoadSceneMode.Additive);
+                break;
+            case Scr_Levels.LevelToLoad.PlanetSystem2:
+                SceneManager.LoadSceneAsync(LevelStorage.planetSystem2, LoadSceneMode.Additive);
+                break;
+            case Scr_Levels.LevelToLoad.PlanetSystem3:
+                SceneManager.LoadSceneAsync(LevelStorage.planetSystem3, LoadSceneMode.Additive);
+                break;
+            case Scr_Levels.LevelToLoad.PlanetSystem4:
+                SceneManager.LoadSceneAsync(LevelStorage.planetSystem4, LoadSceneMode.Additive);
+                break;
+            case Scr_Levels.LevelToLoad.PlanetSystem5:
+                SceneManager.LoadSceneAsync(LevelStorage.planetSystem5, LoadSceneMode.Additive);
+                break;
+            case Scr_Levels.LevelToLoad.PlanetSystem6:
+                SceneManager.LoadSceneAsync(LevelStorage.planetSystem6, LoadSceneMode.Additive);
+                break;
+            case Scr_Levels.LevelToLoad.PlanetSystem7:
+                SceneManager.LoadSceneAsync(LevelStorage.planetSystem7, LoadSceneMode.Additive);
+                break;
+        }
+
+        SceneManager.sceneLoaded += FinishLoading;
+    }
+
+    private void FinishLoading(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.sceneLoaded -= FinishLoading;
+        Destroy(this.gameObject);
+    }
+}
