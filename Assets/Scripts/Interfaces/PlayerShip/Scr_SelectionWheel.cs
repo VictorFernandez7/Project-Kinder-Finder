c3a0deb77fc61a6d589ea141665d7d453f63f989
VictorFernandez7 <victorfernandez95@hotmail.com>
2019-02-10 12:35:26 +0100

    Selection wheel - 12:35

---
+++ Assets/Scripts/Interfaces/PlayerShip/Scr_SelectionWheel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Scr_SelectionWheel : MonoBehaviour
+{
+    [Header("Interaction Properties")]
+    [SerializeField] private float rotSpeed;
+
+    private void OnMouseDrag()
+    {
+        float rotZ = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.forward, -rotZ, Space.World);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnMouseUp()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
---
+++ Assets/Scripts/Interfaces/PlayerShip/Scr_SetActiveButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_SetActiveButton : MonoBehaviour
+{
+    [Header("Select GameObjects")]
+    [SerializeField] private GameObject[] setActiveObjects;
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (setActiveObjects.Length > 0)
+            {
+                for (int i = 0; i < setActiveObjects.Length; i++)
+                {
+                    setActiveObjects[i].SetActive(!setActiveObjects[i].activeInHierarchy);
+                }
+            }
+        }
+    }
+}
