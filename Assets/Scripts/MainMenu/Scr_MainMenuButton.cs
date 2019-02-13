ffb9ce72293e626e97e8af4439c413f7ad9402bc
VictorFernandez7 <victorfernandez95@hotmail.com>
2019-02-13 18:39:45 +0100

    Set up planet buttons - 18:39

--- Assets/Scripts/MainMenu/Scr_MainMenuButton.cs
+++ Assets/Scripts/MainMenu/Scr_MainMenuButton.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.EventSystems;
 using UnityEngine;
+using TMPro;
 
 public class Scr_MainMenuButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    [SerializeField] private GameObject visuals;
+    [SerializeField] private Animator canvasAnim;
+    [SerializeField] private TextMeshProUGUI buttonText;
+
+    private Scr_ButtonVisuals buttonVisuals;
+
+    private void Start()
     {
-        
+        buttonVisuals = GetComponentInChildren<Scr_ButtonVisuals>();
+
+        GetComponent<SphereCollider>().radius = visuals.transform.localScale.x / 100;
+        buttonText.color = Color.clear;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseEnter()
     {
-        
+        buttonVisuals.rotate = true;
+    }
+
+    private void OnMouseExit()
+    {
+        buttonVisuals.rotate = false;
     }
 }
