44320ae3905ed2c842efe964fd19663f799a44a8
SrAlexen <36672804+SrAlexen@users.noreply.github.com>
2019-03-02 22:56:19 +0100

    GetResources - 22:56

--- Assets/Scripts/Resources/Scr_Resource.cs
+++ Assets/Scripts/Resources/Scr_Resource.cs
@@ -25,10 +25,11 @@ public class Scr_Resource : MonoBehaviour
     [SerializeField] private Transform rayOrigin2;
 
     [HideInInspector] public GameObject resourceReference;
+    [HideInInspector] public bool onHands;
 
     private bool emulatePhysics;
     private float spawnAngle;
-    private Rigidbody rb;
+    private Rigidbody2D rb;
     private Scr_PlayerShipMovement playerShipMovement;
 
     public enum ResourceType
@@ -40,9 +41,10 @@ public class Scr_Resource : MonoBehaviour
     private void Start()
     {
         playerShipMovement = GameObject.Find("PlayerShip").GetComponent<Scr_PlayerShipMovement>();
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
 
         emulatePhysics = true;
+        onHands = false;
 
         ResourceTypeChoice();
         InitialImpulse();
@@ -50,22 +52,28 @@ public class Scr_Resource : MonoBehaviour
 
     private void FixedUpdate()
     {
-        activationDelay -= Time.deltaTime;
-
-        if (activationDelay <= 0)
+        if (!onHands)
         {
-            RaycastHit2D hit1 = Physics2D.Raycast(rayOrigin1.position, -transform.up, rayLength, collisionMask);
-            RaycastHit2D hit2 = Physics2D.Raycast(rayOrigin1.position, -transform.up, rayLength, collisionMask);
+            activationDelay -= Time.deltaTime;
 
-            if (hit1 && hit2)
-                rb.isKinematic = true;
-
-            else
+            if (activationDelay <= 0)
             {
-                rb.AddForce((playerShipMovement.currentPlanet.transform.position - gameObject.transform.position).normalized * gravity);
-                rb.isKinematic = false;
+                RaycastHit2D hit1 = Physics2D.Raycast(rayOrigin1.position, -transform.up, rayLength, collisionMask);
+                RaycastHit2D hit2 = Physics2D.Raycast(rayOrigin1.position, -transform.up, rayLength, collisionMask);
+
+                if (hit1 && hit2)
+                    rb.isKinematic = true;
+
+                else
+                {
+                    rb.AddForce((playerShipMovement.currentPlanet.transform.position - gameObject.transform.position).normalized * gravity);
+                    rb.isKinematic = false;
+                }
             }
         }
+
+        else
+            rb.isKinematic = true;
     }
 
     private void ResourceTypeChoice()
