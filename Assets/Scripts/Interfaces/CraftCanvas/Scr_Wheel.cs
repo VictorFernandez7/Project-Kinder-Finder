b1305ddc3ac1c033dee00dbc6b17b310b539789d
VictorFernandez7 <victorfernandez95@hotmail.com>
2019-03-11 20:47:40 +0100

    Animaciones show / hide - 20:47

---
+++ Assets/Scripts/Interfaces/CraftCanvas/Scr_Wheel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Scr_Wheel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("Selection Icons")]
+    [SerializeField] public GameObject[] selectionIcons;
+
+    [Header("Selection Sprites")]
+    [SerializeField] public GameObject[] selectionSprites;
+
+    [Header("References")]
+    [SerializeField] private GameObject wheel;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        wheel.transform.localScale = Vector3.zero;
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        anim.SetBool("Show", true);
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        anim.SetBool("Show", false);
+    }
+}
