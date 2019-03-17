39c502099b8ff23c3dedb295a1675100b65e8480
Víctor Fernández <victorfernandez95@hotmail.com>
2019-03-18 00:31:03 +0100

    Apertura / cierre - 0:30

--- Assets/Scripts/Interfaces/MainCanvas/PlayerShip/Scr_OpenButton.cs
+++ Assets/Scripts/Interfaces/MainCanvas/PlayerShip/Scr_OpenButton.cs
@@ -3,31 +3,52 @@ using UnityEngine;
 
 public class Scr_OpenButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    [Header("Button Type")]
+    [SerializeField] private ButtonType buttonType;
+
     [Header("References")]
     [SerializeField] private Animator playerShipWindowAnim;
     [SerializeField] private Scr_InterfaceManager interfaceManager;
 
     private Animator openButtonAnim;
 
+    private enum ButtonType
+    {
+        Open,
+        Close
+    }
+
     private void Start()
     {
-        openButtonAnim = GetComponent<Animator>();
+        if (buttonType == ButtonType.Open)
+            openButtonAnim = GetComponent<Animator>();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        openButtonAnim.SetBool("ShowText", true);
+        if (buttonType == ButtonType.Open)
+            openButtonAnim.SetBool("ShowText", true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        openButtonAnim.SetBool("ShowText", false);
+        if (buttonType == ButtonType.Open)
+            openButtonAnim.SetBool("ShowText", false);
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        playerShipWindowAnim.SetBool("Show", true);
-        openButtonAnim.SetBool("ShowText", false);
-        interfaceManager.PlayerShipWindow();
+        if (buttonType == ButtonType.Open)
+        {
+            playerShipWindowAnim.SetBool("Show", true);
+            openButtonAnim.SetBool("ShowText", false);
+            interfaceManager.PlayerShipWindow();
+        }
+
+        else if (buttonType == ButtonType.Close)
+        {
+            playerShipWindowAnim.SetBool("Show", false);
+            interfaceManager.PlayerShipWindow();
+        }
     }
 }
