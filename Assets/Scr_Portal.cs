4743a48181ab8c0f0fd1eb29e19ea9b230e2906c
SrAlexen <36672804+SrAlexen@users.noreply.github.com>
2019-03-19 17:37:42 +0100

    Portal - 17:37

---
+++ Assets/Scr_Portal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Portal : MonoBehaviour
+{
+    private bool onRange;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerShip"))
+            onRange = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerShip"))
+            onRange = false;
+    }
+
+    private void Update()
+    {
+        if (onRange && Input.GetKeyDown(KeyCode.E))
+            Scr_LevelManager.LoadSystemSelection();
+    }
+}
