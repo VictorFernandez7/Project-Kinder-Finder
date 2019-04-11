8f5ed0d3c487112522e2129f6d55a43ce4fe6555
SrAlexen <36672804+SrAlexen@users.noreply.github.com>
2019-04-11 12:26:47 +0200

    BugSmashing - 12:25
    -Jump in dialogs Bug
    -Resource Astronaut Check Bug
    -Click to quick dialogues

--- Assets/Scripts/Player/Astronaut/Scr_AstronautResourcesCheck.cs
+++ Assets/Scripts/Player/Astronaut/Scr_AstronautResourcesCheck.cs
@@ -5,11 +5,35 @@ using UnityEngine;
 public class Scr_AstronautResourcesCheck : MonoBehaviour
 {
     public List<GameObject> resourceList = new List<GameObject>();
+    private int count = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            for(int i = 0; i < resourceList.Count; i++)
+            {
+                print(i + " >>  " + resourceList[i].GetComponent<Scr_Resource>().iD);
+            }
+        }
+
+        if (count != resourceList.Count)
+        {
+            for (int i = 0; i < resourceList.Count; i++)
+            {
+                resourceList[i].GetComponent<Scr_Resource>().iD = i;
+                count = resourceList.Count;
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Resources"))
+        {
             resourceList.Add(collision.gameObject);
+        }
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -18,7 +42,7 @@ public class Scr_AstronautResourcesCheck : MonoBehaviour
         {
             for (int i = 0; i < resourceList.Count; i++)
             {
-                if(resourceList[i].name == collision.gameObject.name)
+                if(resourceList[i].name == collision.gameObject.name && resourceList[i].GetComponent<Scr_Resource>().iD == collision.gameObject.GetComponent<Scr_Resource>().iD)
                     resourceList.RemoveAt(i);
             }
         }
