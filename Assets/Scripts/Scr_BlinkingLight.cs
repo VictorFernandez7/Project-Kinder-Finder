2cd51cd3f2f483435d717b22a3159abd9a3b88f6
Víctor Fernández <victorfernandez95@hotmail.com>
2019-05-05 23:22:54 +0200

    Space base nueva terminada - 23:22

---
+++ Assets/Scripts/Scr_BlinkingLight.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_BlinkingLight : MonoBehaviour
+{
+    [Header("Light Parameters")]
+    [SerializeField] private float minTime;
+    [SerializeField] private float maxTime;
+
+    private float timeToBlink;
+    private bool blinking;
+    private Light spotLight;
+
+    void Start()
+    {
+        spotLight = GetComponent<Light>();
+
+        timeToBlink = Random.Range(minTime, maxTime);
+    }
+
+    void Update()
+    {
+        if (!blinking)
+            timeToBlink -= Time.deltaTime;
+
+        if (timeToBlink <= 0)
+        {
+            blinking = true;
+            timeToBlink = Random.Range(minTime, maxTime);
+            Blink();
+        }
+    }
+
+    private void Blink()
+    {
+        spotLight.enabled = false;
+
+        Invoke("TurnOnLight", 0.5f);
+    }
+
+    private void TurnOnLight()
+    {
+        spotLight.enabled = true;
+        blinking = false;
+    }
+}
