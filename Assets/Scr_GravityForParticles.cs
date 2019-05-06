afc2ec91ae7adee0d9376ded89f5a1c1800c904e
Víctor Fernández <victorfernandez95@hotmail.com>
2019-05-06 11:27:54 +0200

    Script gravedad para partículas + collider liquid ore - 11:27

--- Assets/Scr_GravityForParticles.cs
+++ Assets/Scr_GravityForParticles.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class Scr_GravityForParticles : MonoBehaviour
 {
-    private ParticleSystem particles;
+    [Header("Gravity Parameters")]
+    [SerializeField] private float gravityAmount;
+
+    [Header("References")]
+    [SerializeField] private Scr_PlayerShipMovement playerShipMovement;
+
+    private int numberOfParticles;
+    private Vector3 desiredDirection;
+    private ParticleSystem liquidParticles;
+    private ParticleSystem.Particle[] currentParticles;
+
+    private void Start()
+    {
+        liquidParticles = GetComponent<ParticleSystem>();
+        currentParticles = new ParticleSystem.Particle[liquidParticles.main.maxParticles];
+    }
 
     private void Update()
     {
-        
+        numberOfParticles = liquidParticles.GetParticles(currentParticles);
+        desiredDirection = playerShipMovement.currentPlanet.transform.position - transform.position;
+
+        for (int i = 0; i < numberOfParticles; i++)
+        {
+            currentParticles[i].velocity += desiredDirection * gravityAmount * Time.deltaTime;
+        }
+
+        liquidParticles.SetParticles(currentParticles, numberOfParticles);
     }
 }
