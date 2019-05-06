220c0e60874e4fbc755bcdb3d4698e6f58ab298a
Víctor Fernández <victorfernandez95@hotmail.com>
2019-05-06 04:58:28 +0200

    Luz implementada en la IA - 4:58

---
+++ Assets/Scripts/Player/IA/Scr_IALight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_IALight : MonoBehaviour
+{
+    private Light spotLight;
+    private GameObject astronaut;
+    private Scr_SunLight sunLight;
+
+    void Start()
+    {
+        astronaut = GameObject.Find("Astronaut");
+        sunLight = GameObject.Find("SunLight").GetComponent<Scr_SunLight>();
+        spotLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        LightDirection();
+        LightActivation();
+    }
+
+    private void LightDirection()
+    {
+        Vector3 targetRotation = astronaut.transform.position - transform.position;
+
+        transform.forward = targetRotation;
+    }
+
+    private void LightActivation()
+    {
+        spotLight.enabled = !sunLight.hitByLight;
+    }
+}
