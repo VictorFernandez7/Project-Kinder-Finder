fb9e4432a78d0091eb79b1a705fb3bf58d3097bd
Víctor Fernández <victorfernandez95@hotmail.com>
2019-05-09 00:41:35 +0200

    Evento romper puerta vestido - 0:41

--- Assets/Scripts/PlayScene/PlanetSystem/Resources/Ores/Scr_BreakeableDetection.cs
+++ Assets/Scripts/PlayScene/PlanetSystem/Resources/Ores/Scr_BreakeableDetection.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Scr_BreakeableDetection : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private GameObject inputText;
     [SerializeField] private Scr_AstronautsActions astronautsActions;
+
+    private bool insideTrigger;
+
+    private void Update()
+    {
+        CanvasItemActivation();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Astronaut"))
         {
+            insideTrigger = true;
+
             astronautsActions.miningSpot = this.gameObject;
             astronautsActions.spotType = Scr_AstronautsActions.SpotType.breakeable;
         }
@@ -19,6 +30,27 @@ public class Scr_BreakeableDetection : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Astronaut"))
+        {
+            insideTrigger = false; ;
+
             astronautsActions.miningSpot = null;
+        }
+    }
+
+    private void CanvasItemActivation()
+    {
+        if (insideTrigger)
+        {
+            if (astronautsActions.iAMovement.isMining)
+                inputText.SetActive(false);
+
+            else if (astronautsActions.solidTool.activeInHierarchy)
+            {
+                inputText.SetActive(true);
+            }
+        }
+
+        else
+            inputText.SetActive(false);
     }
 }
