cc376a0f3513d98e76a1c0507f50918c508f2236
Víctor Fernández <victorfernandez95@hotmail.com>
2019-05-11 00:41:23 +0200

    Indicadores - 0:41

---
+++ Assets/Scripts/PlayScene/Characters/Indicators/Scr_MiniMapIndicator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_MiniMapIndicator : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Scr_PlayerShipMovement playerShipMovement;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (playerShipMovement.currentPlanet != null)
+            spriteRenderer.enabled = true;
+
+        else
+            spriteRenderer.enabled = false;
+    }
+}
