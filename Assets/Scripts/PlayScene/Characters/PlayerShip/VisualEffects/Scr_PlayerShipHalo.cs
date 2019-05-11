e2bbb5dbef330d7ee122cd4301e78708ff8e4135
Víctor Fernández <victorfernandez95@hotmail.com>
2019-05-11 04:15:30 +0200

    Aparición / desaparición - 4:15

--- Assets/Scripts/PlayScene/Characters/PlayerShip/VisualEffects/Scr_PlayerShipHalo.cs
+++ Assets/Scripts/PlayScene/Characters/PlayerShip/VisualEffects/Scr_PlayerShipHalo.cs
@@ -4,102 +4,104 @@ using UnityEngine;
 
 public class Scr_PlayerShipHalo : MonoBehaviour
 {
-    [Header("Halo Sttings")]
+    [Header("Halo Parameters")]
     [SerializeField] private float radius;
     [SerializeField] private float width;
-    [SerializeField] private float colorLerpSpeed;
-    [SerializeField] private float colorChangeDelay;
-    [SerializeField] private Color activeHalo;
-    [SerializeField] private Color notActiveHalo;
+
+    [Header("Color Parameters")]
+    [SerializeField] private Color inSpace;
+    [SerializeField] private Color inPlanet;
+    [SerializeField] private float lerpSpeed;
+
+    [Header("Delays")]
+    [SerializeField] private float takingOffDelay;
 
     [Header("References")]
     [SerializeField] private Transform playership;
-    [SerializeField] private Scr_PlayerShipMovement playerShipMovement;
 
-    private float initialTimerValue;
-    private LineRenderer haloLine;
+    private float savedTimer;
+    private Color targetColor;
+    private LineRenderer lineRenderer;
+    private Scr_PlayerShipMovement playerShipMovement;
 
     private void Start()
     {
-        haloLine = GetComponent<LineRenderer>();
-        initialTimerValue = colorChangeDelay;
+        playerShipMovement = playership.GetComponent<Scr_PlayerShipMovement>();
+        lineRenderer = GetComponent<LineRenderer>();
+
+        savedTimer = takingOffDelay;
     }
 
     void Update()
     {
-        HaloColor();
         HaloPoints();
         HaloProperties();
-    }
-
-    private void HaloColor()
-    {
-        if (playerShipMovement.playerShipState == Scr_PlayerShipMovement.PlayerShipState.landed)
-        {
-            haloLine.startColor = notActiveHalo;
-            haloLine.endColor = notActiveHalo;
-
-            colorChangeDelay = initialTimerValue;
-        }
-
-        if (playerShipMovement.playerShipState == Scr_PlayerShipMovement.PlayerShipState.landing)
-        {
-            haloLine.startColor = Color.Lerp(haloLine.startColor, notActiveHalo, Time.deltaTime * colorLerpSpeed);
-            haloLine.endColor = Color.Lerp(haloLine.endColor, notActiveHalo, Time.deltaTime * colorLerpSpeed);
-        }
-
-        if (playerShipMovement.playerShipState == Scr_PlayerShipMovement.PlayerShipState.takingOff || playerShipMovement.playerShipState == Scr_PlayerShipMovement.PlayerShipState.inSpace)
-        {
-            colorChangeDelay -= Time.deltaTime;
-
-            if (colorChangeDelay <= 0)
-            {
-                haloLine.startColor = Color.Lerp(haloLine.startColor, activeHalo, Time.deltaTime * colorLerpSpeed);
-                haloLine.endColor = Color.Lerp(haloLine.endColor, activeHalo, Time.deltaTime * colorLerpSpeed);
-            }
-        }
+        HaloAlphaControl();
     }
 
     private void HaloPoints()
     {
         int index = 0;
 
-        haloLine.positionCount = 41;
+        lineRenderer.positionCount = 41;
 
         for (float i = 1; i >= 0; i -= 0.1f)
         {
             Vector3 vectorDirector = new Vector3(i, 1 - i, 0);
-            haloLine.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
+            lineRenderer.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
             index += 1;
         }
 
         for (float i = 0; i >= -1; i -= 0.1f)
         {
             Vector3 vectorDirector = new Vector3(i, 1 + i, 0);
-            haloLine.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
+            lineRenderer.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
             index += 1;
         }
 
         for (float i = -1; i <= 0; i += 0.1f)
         {
             Vector3 vectorDirector = new Vector3(i, -1 - i, 0);
-            haloLine.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
+            lineRenderer.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
             index += 1;
         }
 
         for (float i = 0; i <= 1; i += 0.1f)
         {
             Vector3 vectorDirector = new Vector3(i, -1 + i, 0);
-            haloLine.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
+            lineRenderer.SetPosition(index, (vectorDirector.normalized * radius) + playership.position);
             index += 1;
         }
 
-        haloLine.SetPosition(40, (new Vector3(1, 0, 0) * radius) + playership.position);
+        lineRenderer.SetPosition(40, (new Vector3(1, 0, 0) * radius) + playership.position);
     }
 
     private void HaloProperties()
     {
-        haloLine.startWidth = width;
-        haloLine.endWidth = width;
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+    }
+
+    private void HaloAlphaControl()
+    {
+        if (playerShipMovement.playerShipState == Scr_PlayerShipMovement.PlayerShipState.landed)
+        {
+            targetColor = inPlanet;
+            savedTimer = takingOffDelay;
+        }
+
+        if (playerShipMovement.playerShipState == Scr_PlayerShipMovement.PlayerShipState.landing)
+            targetColor = inPlanet;
+
+        if (playerShipMovement.playerShipState == Scr_PlayerShipMovement.PlayerShipState.takingOff || playerShipMovement.playerShipState == Scr_PlayerShipMovement.PlayerShipState.inSpace)
+        {
+            savedTimer -= Time.deltaTime;
+
+            if (savedTimer <= 0)
+                targetColor = inSpace;
+        }
+
+        lineRenderer.startColor = Color.Lerp(lineRenderer.startColor, targetColor, Time.deltaTime * lerpSpeed);
+        lineRenderer.endColor = Color.Lerp(lineRenderer.endColor, targetColor, Time.deltaTime * lerpSpeed);
     }
 }
